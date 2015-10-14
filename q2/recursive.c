d4418a36d7ee7ea1c56ce51670c6eb1c5ac6f066
vodkaice <brandedwolf@gmal.com>
2015-10-14 23:40:40 +0800

    complete basic version of recursive
    it fix input['c','f','j','p','v'] and comparation word c='m'
    compare from head to tail,so its time complexity is O(n)

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,24 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+char smallest_character(char *input ,char c) {
+    if(*input<c){
+	return smallest_character(++input,c);
+    }
+    else{
+	return *input;
+    }
+}
 
 int main()
 {
+    char input[]={'c','f','j','p','v'},c='m',output;
+    
+    assert((smallest_character(input,c)) && "function error!\n");
+    output = smallest_character(input, c);
+    printf("result : %c\n",output);
     return 0;
 }
