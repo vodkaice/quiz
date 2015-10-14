461c0c601ccf9372dc623cc250e936dd8f357ce2
vodkaice <brandedwolf@gmal.com>
2015-10-14 23:31:25 +0800

    delete the reundant function

--- q2/iterative.c
+++ q2/iterative.c
@@ -20,8 +20,6 @@ int main()
 {
     char input[]={'c','f','j','p','v'},c='m',output;
     
-    int cmp=strcmp(input,&c);
-    printf("cmp : %d\n",cmp);
     assert((smallest_character(input,c)) && "function error!\n");
     output = smallest_character(input, c);
     printf("result : %c\n",output);
