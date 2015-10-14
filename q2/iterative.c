9c6e4591fd5c1521a984f818662c31cee6e3057d
vodkaice <brandedwolf@gmal.com>
2015-10-14 23:06:01 +0800

    complete basic version of iteration
    it fix input['c','f','j','p','v'] and comparation word c='m'
    compare from head to tail,so its time complexity is O(n)

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,35 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
+char smallest_character(char *input ,char c) {
+    char res;	
+    //printf("%c\n",*input++);
+    //printf("%c\n",*input);
+    int i=0;
+    while(*input!='\0'){
+	if(*input < c){
+	    *input++;
+	}
+	else{
+	    res = *input;
+	    break;
+	}
+	//*input++;
+    }
+    
+    return res;
+}
 int main()
 {
+    char input[]={'c','f','j','p','v'},c='m',output;
+    
+    int cmp=strcmp(input,&c);
+    printf("cmp : %d\n",cmp);
+    assert((smallest_character(input,c)) && "function error!\n");
+    output = smallest_character(input, c);
+    printf("result : %c\n",output);
     return 0;
 }
