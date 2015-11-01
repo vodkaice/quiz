7d286758beb45074400bf77b8de2988fd0b899ff
vodkaice <brandedwolf@gmal.com>
2015-11-01 22:42:45 +0800

    implement by changing the link of nodes which visited to _head
    when we visit the node, we check if the next node point to _head
    (_head is a global variable that help every time in detect function can point to)
    if not true, we change the link of this node to head, then visit next node
    if true, the next point is the begin of the cycle we want to find.

--- q5/recursive.c
+++ q5/recursive.c
@@ -1,6 +1,80 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+typedef struct link{
+    int data;
+    struct link *next;
 
+}LinkNode;
+ /* Link
+              n6  <----     n5
+               |             |
+        n1 -> n2 -> n3 -> n4 |
+ 	
+ */
+LinkNode *_head;
+LinkNode *CreateNode(){
+    LinkNode *node1 = (LinkNode *)malloc(sizeof(LinkNode));
+    node1->data = 1;
+    node1->next = (LinkNode *)malloc(sizeof(LinkNode));
+    
+    LinkNode *node2 = node1->next;
+    node2->data = 2;
+    node2->next = (LinkNode *)malloc(sizeof(LinkNode));
+
+    LinkNode *node3 = node2->next;
+    node3->data = 3;
+    node3->next = (LinkNode *)malloc(sizeof(LinkNode));;
+
+    LinkNode *node4 = node3->next;
+    node4->data = 4;
+    node4->next = (LinkNode *)malloc(sizeof(LinkNode));;
+
+    LinkNode *node5 = node4->next;
+    node5->data = 5;
+    node5->next = (LinkNode *)malloc(sizeof(LinkNode));;
+
+    LinkNode *node6 = node5->next;
+    node6->data = 6;
+    node6->next = node2;
+
+    return node1;
+}
+LinkNode *detectCycle(LinkNode *head){
+    assert((head->next != NULL && head->next->next !=NULL) && "no cycle");
+    LinkNode *now = head;
+    LinkNode *temp = now->next;
+    if(temp->next != _head && temp->next != NULL){
+	now->next = _head;
+	now = temp;
+	temp = temp->next;   
+	return detectCycle(now);
+    }
+    else if(temp->next == NULL){
+	printf("no cycle\n");
+	return NULL;
+    }
+    else{
+        printf("cycle detected!\n"); 
+	return temp;
+    }
+}
 int main()
 {
+    LinkNode *head = CreateNode();
+    _head = head;
+    LinkNode *res = detectCycle(head);
+    //use print to check the list 
+    /*printf("%d->%d->%d->%d->%d->%d->%d\n",head->data,
+				    head->next->data,
+				    head->next->next->data,
+				    head->next->next->next->data,
+				    head->next->next->next->next->data,
+				    head->next->next->next->next->next->data,
+				    head->next->next->next->next->next->next->data);*/
+    if (res != NULL)
+	printf("result: n%d\n",res->data);
+    else
+	printf("NULL\n");
     return 0;
 }
