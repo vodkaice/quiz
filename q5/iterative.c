74ca81e06dad267848d994bc36c3879bffab147e
vodkaice <brandedwolf@gmal.com>
2015-11-03 22:24:48 +0800

    add floyd alogrithm to detect cycle, and compare the execution time of two method
    implement floyd alogrithm,
    then use #if defined() to generate two binary file to compare the execution time

--- q5/iterative.c
+++ q5/iterative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <time.h>
 typedef struct link{
     int data;
     struct link *next;
@@ -39,6 +40,37 @@ LinkNode *CreateNode(){
 
     return node1;
 }
+#if defined(FLOYD)
+LinkNode *detectLoop(LinkNode *head){
+    LinkNode *fast, *slow;
+    fast = slow = head;
+    while( fast && fast->next && fast->next->next){
+	fast = fast->next->next;
+	slow = slow->next;
+	if(fast == slow)
+	    return slow;
+    }
+    return NULL;    //no loop exist
+}
+LinkNode *detectCycle(LinkNode *head){
+    LinkNode *meet_point = detectLoop(head);
+    LinkNode *start_head = NULL;    //node start from head
+    LinkNode *start_meet = NULL;    //node start from meet point
+    
+    if(meet_point == NULL)
+	return NULL;
+    else{ 
+	start_head = head;
+	start_meet = meet_point;
+	while (start_head && start_meet && start_head != start_meet){
+	    start_head = start_head->next;
+	    start_meet = start_meet->next;
+	}
+    }
+    return start_head; 
+}
+#endif
+#if defined(DESTROY)
 LinkNode *detectCycle(LinkNode *head){
     assert((head->next != NULL && head->next->next !=NULL) && "no cycle");
     LinkNode *now = head;
@@ -48,24 +80,42 @@ LinkNode *detectCycle(LinkNode *head){
 	    now->next = head;
 	    now = temp;
 	    temp = temp->next;
-	    
 	}
 	else if(temp->next == NULL){
 	    temp = NULL;
-	    printf("no cycle\n");
+	    //printf("no cycle\n");
 	    break;
 	}
 	else{
-	    printf("cycle detected!\n"); 
+	    //printf("cycle detected!\n"); 
 	    break;
 	}
     }
     return temp;
 }
+#endif
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
 int main()
 {
+    struct timespec start, end;
+    double cpu_time;
     LinkNode *head = CreateNode();
+    assert((head->next != NULL && head->next->next !=NULL) && "no cycle");
+    clock_gettime(CLOCK_REALTIME, &start);
     LinkNode *res = detectCycle(head);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time = diff_in_second(start, end);
     //use print to check the list 
     /*printf("%d->%d->%d->%d->%d->%d->%d\n",head->data,
 				    head->next->data,
@@ -75,7 +125,8 @@ int main()
 				    head->next->next->next->next->next->data,
 				    head->next->next->next->next->next->next->data);*/
     if (res != NULL)
-	printf("result: n%d\n",res->data);
+	//printf("result: n%d\n",res->data);
+	printf("execution time: %.10lf sec\n", cpu_time);
     else
 	printf("NULL\n");
     return 0;
